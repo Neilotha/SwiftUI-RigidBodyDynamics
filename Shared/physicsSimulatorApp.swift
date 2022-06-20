deeabd667858de49c4695e2deabcc253c644d171
Joshua Yang <joshuayang@mac.com>
2022-06-20 13:42:44 +0800

    Initial Commit

---
+++ Shared/physicsSimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  physicsSimulatorApp.swift
+//  Shared
+//
+//  Created by Joshua Yang on 2022/6/20.
+//
+
+import SwiftUI
+
+@main
+struct physicsSimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
